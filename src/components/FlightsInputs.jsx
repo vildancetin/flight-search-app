df7a44da2ab513a2d02be03a2eccc03a65c5dde5
Vildan Beyza Çetin <vcetin1356@gmail.com>
2024-01-26 00:00:17 +0300

    inputs added

--- src/components/FlightsInputs.jsx
+++ src/components/FlightsInputs.jsx
@@ -1,5 +1,29 @@
+
+import { Label, TextInput } from 'flowbite-react';
+import { Datepicker } from 'flowbite-react';
 const FlightsInputs = () => {
-  return <div>FlightsInputs</div>;
+  return (
+    <>
+    <div className='flex justify-center gap-8 mb-5'>
+      <div>
+        <div className="mb-2 block">
+          <Label htmlFor="base" value="Departure" />
+        </div>
+        <TextInput id="base" type="text" sizing="md" className='w-[500px]' />
+      </div>
+      <div>
+        <div className="mb-2 block">
+          <Label htmlFor="base" value="Destination" />
+        </div>
+        <TextInput id="base" type="text" sizing="md" className='w-[500px]' />
+      </div>
+    </div>
+    <div className='flex justify-center gap-8'>
+        <Datepicker className='w-[500px]' />
+        <Datepicker className='w-[500px]' />
+    </div>
+    </>
+  );
 };
 
 export default FlightsInputs;
